f6892796263a659eaa15fd9b7c279d1f5c5740e7
pedromfm02 <pedromfm02@ua.pt>
2023-10-03 15:44:48 +0100

    lab2_1 completed

---
+++ Lab2/lab2_1/src/main/java/pedro/ies/EmbeddedJettyExample.java
@@ -0,0 +1,47 @@
+package pedro.ies;
+  
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+ 
+public class EmbeddedJettyExample {
+ 
+    public static void main(String[] args) throws Exception {
+         
+        Server server = new Server(8680);               
+         
+        ServletHandler servletHandler = new ServletHandler();
+        server.setHandler(servletHandler);
+                 
+        servletHandler.addServletWithMapping(HelloServlet.class, "/");
+         
+        server.start();
+        server.join();
+ 
+    }
+     
+    public static class HelloServlet extends HttpServlet 
+    {
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+        {
+            response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+
+            String msg = request.getParameter("msg");
+            if(msg == null){
+                response.getWriter().println("<h1>New Hello Simple Servlet</h1>"); 
+            }else{
+                response.getWriter().println("<h1>"+msg+"</h1>"); 
+            }
+            
+        } 
+    }
+ }
